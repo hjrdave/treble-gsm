75e0418f8987477c0d4d82c03d31ad170d947b88
David Sanders <dsanders_m633@outlook.com>
2020-03-28 23:57:02 -0400

    added more debug warnings for Store.js

--- src/treble-debug/treble-debug.tsx
+++ src/treble-debug/treble-debug.tsx
@@ -22,39 +22,67 @@ interface ITrebleDebug {
 export const trebleDebug: ITrebleDebug = ({ data }) => {
 
     const trebleError = 'Treble Error:';
+    const trebleWarn = 'Treble Warning:';
     const featuresList = ['persist', 'call', 'check', 'convert'];
 
-    data.forEach(({ action, state, features }) => {
-        //checks Store.action type
+    data.forEach(({ action, state, features }, index) => {
+         
+        /** Store.js warnings and errors */
+         
+        //checks Store action type
         if (typeof action !== 'string') {
-            console.error(`${trebleError} Store.action must be of type 'string'.`);
+            console.error(`${trebleError} Store[${index}].action must be a string.`)
         }
 
-        //checks Store.state type
+        //checks Store state type
         if (typeof state !== 'object') {
-            console.error(`${trebleError} Store.state must be of type 'object'.`);
+            console.error(`${trebleError} Store[${index}].state must be an object.`)
         }
 
-        //checks to see if Store.state has multiple properties
+        //checks to see if Store state is empty
+        if (!(Object.keys(state).length > 0) && typeof state === 'object') {
+            console.warn(`${trebleWarn} Store[${index}].state must have one property.`);
+        }
+
+        //checks to see if Store state has multiple properties
         if (Object.keys(state).length > 1 && typeof state === 'object') {
-            console.error(`${trebleError} Store.state can only have one property.`);
+            console.error(`${trebleError} Store[${index}].state can only have one property.`);
         }
 
-        //checks Store.features 
-        if (features) {
-
+        //checks Store features 
+       if(features !== undefined){
             //checks type
             if (typeof features !== 'object') {
-                console.error(`${trebleError} Store.features must be of type 'object'.`);
+                console.error(`${trebleError} Store[${index}].features must be an object. `);
             }
-
+            
             //makes sure props are legitimate
             Object.keys(features).forEach((feature) => {
                 if (featuresList.includes(feature) !== true) {
-                    console.error(`${trebleError} Store.features.${feature} is not a valid treble feature.`);
+                    console.error(`${trebleError} Store[${index}].features property ${feature} is not a valid property.`);
                 }
             });
 
-        }
+            //checks property persist
+            if(typeof features.persist !== 'boolean' && features.persist !== undefined){
+                console.error(`${trebleError} Store.[${index}].features[${index}].persist must be a boolean.`);
+            }
+
+            //checks property call
+            if(typeof features.call !== 'function' && features.call !== undefined){
+                console.error(`${trebleError} Store[${index}].call must be a function.`);
+            }
+
+            //checks property check
+            if(typeof features.check !== 'function' && features.check !== undefined){
+                console.error(`${trebleError} Store[${index}].features.check must be a function.`);
+            } 
+
+            //checks property convert
+            if(typeof features.convert !== 'function' && features.convert !== undefined){
+                console.error(`${trebleError} Store[${index}].features.convert must be a function.`);
+            } 
+       }
+      
     })
 };
