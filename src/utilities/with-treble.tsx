ef1227665c2b2d81910fc12c2e47cbbcc2a0cedf
David Sanders <dsanders_m633@outlook.com>
2020-04-16 11:25:04 -0400

    added reactClass and store options to withTreble so functional components can use it for scope

--- src/utilities/with-treble.tsx
+++ src/utilities/with-treble.tsx
@@ -6,15 +6,55 @@
 
 import React from 'react';
 import useTreble from '../hooks/treble-hook';
+import Treble from '../treble';
 
-export default function withTreble(Component: any) {
-  return function ClassComponent(props: any) {
-    const store = useTreble()[0];
-    const dispatch = useTreble()[1];
-    const getTreble = [
-      store,
-      dispatch
-    ]
-    return <Component {...props} getTreble={getTreble}/>;
+interface IWithTreble {
+  (Component: any,
+    options?: {
+      reactClass?: boolean,
+      store?:
+        {
+          data: {
+              action: string,
+              state: {
+                  [key: string]: any
+              },
+              features?: {
+                  persist?: boolean
+              }
+          }[],
+          scope?: React.Context<never[]>
+      }
+    }): void
+}
+export const withTreble: IWithTreble = (Component, options) => {
+
+  //returns a class component with getTreble props if reactClass is set to true
+  if (options?.reactClass === true) {
+    return function ClassComponent(props: any) {
+      const store = useTreble()[0];
+      const dispatch = useTreble()[1];
+      const getTreble = [
+        store,
+        dispatch
+      ]
+      return <Component {...props} getTreble={getTreble} />;
+    }
+  }
+  //returns functional component
+  return function FunctionalComponent(props: any) {
+    return (
+      <>
+        {
+          (options?.store !== undefined) ?
+            <Treble store={options?.store}>
+              <Component {...props}/>
+            </Treble>
+            : null
+        }
+      </>
+    )
   }
 }
+
+export default withTreble;
