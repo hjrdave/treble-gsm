872e1a1a4e5440e2dbc334ba5566b8a2b541a5c6
David Sanders <dsanders_m633@outlook.com>
2020-05-06 12:40:24 -0400

    added better typings to create-store handleExtendStore function

--- src/utilities/create-store/create-store.ts
+++ src/utilities/create-store/create-store.ts
@@ -4,6 +4,7 @@
 */
 import {ICreateStore} from './interfaces';
 import errorHandling from './error-handling';
+import {IStoreItem} from '../../interfaces';
 
 const createStore: ICreateStore = (storeData, options) => {
     
@@ -11,18 +12,18 @@ const createStore: ICreateStore = (storeData, options) => {
     errorHandling(storeData);
 
     //handle extendStore array
-    const handleExtendStore = (extendStoreProp: any) => {
+    const handleExtendStore = (extendStoreProp: {data: IStoreItem[]}[]) => {
         //created multidimensional array from extendStore property array
-        let array = extendStoreProp.map((data: any) => {
+        let array = extendStoreProp.map((data: {data: IStoreItem[]}) => {
             return data;
         });
         //flattens array so it can be passed to store.data prop
-        let flattenedArray:any = array.reduce((arr: any, elem: any) => [...arr, ...elem.data],[]);
+        let flattenedArray:IStoreItem[] = array.reduce((arr: any, elem: any) => [...arr, ...elem.data],[]);
         return flattenedArray;
     }
 
     //Checks to see if extendStore option exists and then processes it so it can be added to store.data prop
-    let extendedStoreData = (options?.extendStore) ? handleExtendStore(options?.extendStore) : false;
+    let extendedStoreData: boolean | IStoreItem[] = (options?.extendStore) ? handleExtendStore(options?.extendStore) : false;
 
     //store object that will be used by Treble
     let  store = {
