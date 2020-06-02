6da10e36c20c37422ca5c26b027159eeadb15668
David Sanders <dsanders_m633@outlook.com>
2020-06-01 23:37:08 -0400

    let Provider be accessible to outside applications. Did this so it could be used by extensions.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Treble from './treble';
+import Provider from './provider';
 import {createStore, updateStore, clearPersist, withTreble} from './utilities';
 import {useTreble, useScopedTreble} from './hooks';
 
 
-export {useTreble, updateStore, createStore, useScopedTreble, withTreble, clearPersist};
+export {useTreble, updateStore, createStore, useScopedTreble, withTreble, clearPersist, Provider};
 export default Treble;
